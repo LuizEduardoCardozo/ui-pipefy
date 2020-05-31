1bfe5e7f046a217c69d8b489c5e1cd6ca038f345
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2020-05-30 23:40:30 -0300

    add: drag and drop update

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import {useDrag} from 'react-dnd';
+import React, {useRef} from 'react';
+import {useDrag, useDrop} from 'react-dnd';
 
 import { Container, Label } from './styles';
 
 function Card({data}) {
+  const ref = useRef();
   const [{isDragging}, dragRef] = useDrag({
-    item: {type: 'CARD'},
+
+    item: {
+      type: 'CARD', 
+      id: data.id,
+    },
+
     collect: monitor => ({
       isDragging: monitor.isDragging()
     }),
+
   });
+
+  const [,dropRef] = useDrop({
+    accept: "CARD",
+    hover(item, monitor){
+      console.log(item.id);
+    },
+  });
+
+  dragRef(dropRef(ref));
 
   return (
       <Container ref={dragRef} isDragging={isDragging}>
